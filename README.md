d5c64611860d6ac49efbfaa83af01ccf838ba83b
JuHeon-Oh <ojhgold@gmail.com>
2022-12-20 01:06:17 +0900

    add: readme about how to use

--- README.md
+++ README.md
@@ -59,3 +59,17 @@ You can use multiple run at once
 ```bash
 python src/train.py datamodule.batch_size=16,32,64,128 model.arch_name="UnetPlusPlus", "Unet"
 ```
+
+## How to use
+
+- train 실행 시 test도 실행하고 csv 파일 자동으로 생성
+   - python src/train.py
+
+- 학습된 ckpt 파일을 이용하여 test 실행하려면
+    - python src/eval.py ckpt_path = "your ckpt path"
+
+- configs폴더에 있는 config들을 CLI 상에서 수정해서 사용 가능
+    - python src/train.py datamodule.batch_size=128 model.arch_name="Unet" model.encoder_name="your encoder name"
+    - model 관련 config는 [Docs](https://smp.readthedocs.io/en/latest/encoders_timm.html) 참고
+- encoder, archtitechtures, loss, optimizer, scheduler 등등 다양한 config 수정 가능
+- 더 자세한 내용은 [README](https://github.com/ashleve/lightning-hydra-template) 참고
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -9,26 +9,5 @@ from src.utils.utils import (
     log_hyperparameters,
     save_file,
     task_wrapper,
+    make_submit
 )
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-def get_confmat(df):
-    df = pd.DataFrame(df.detach().cpu().numpy())
-    plt.clf()  # ADD THIS LINE
-    plt.figure(figsize=(10, 10))
-    confmat_heatmap = sns.heatmap(
-        data=df,
-        cmap="RdYlGn",
-        annot=True,
-        fmt=".3f",
-        cbar=False,
-    )
-    plt.ylabel("True Label")
-    plt.xlabel("Predicted label")
-
-    confmat_heatmap.xaxis.set_label_position("top")
-    plt.yticks(rotation=0)
-    confmat_heatmap.tick_params(axis="x", which="both", bottom=False)
-
-    return confmat_heatmap.get_figure()
