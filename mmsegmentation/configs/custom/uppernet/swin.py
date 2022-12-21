0048179f01d5dde0d11fd51f9aac8eafc22ba343
tec10182 <tec10182@naver.com>
2022-12-21 10:36:35 +0000

    feat: mmsegmentation

---
+++ mmsegmentation/configs/custom/uppernet/swin.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../../_base_/models/upernet_swin.py', '../_base_/dataset.py',
+    '../_base_/default_runtime.py', '../_base_/base_schedule.py'
+]
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_tiny_patch4_window7_224_20220317-1cdeb081.pth'  # noqa
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        use_abs_pos_embed=False,
+        drop_path_rate=0.3,
+        patch_norm=True),
+    decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=11),
+    auxiliary_head=dict(in_channels=384, num_classes=11))
