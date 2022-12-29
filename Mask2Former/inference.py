69081bcc232f801e350137c9c25470bc9f2df13e
JuHeon-Oh <ojhgold@gmail.com>
2022-12-30 01:39:11 +0900

    add: set submission file name for each experiment

--- Mask2Former/inference.py
+++ Mask2Former/inference.py
@@ -4,41 +4,45 @@ import cv2
 
 sys.path.insert(0, "Mask2Former")
 
-from pathlib import Path
-import numpy as np
 import argparse
 import json
-from tqdm import tqdm
-import albumentations as A
-import pandas as pd
-import warnings
 import os
+import re
+import warnings
+from pathlib import Path
+
+import albumentations as A
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
-
-# import some common detectron2 utilities
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.projects.deeplab import add_deeplab_config
-from detectron2.data.detection_utils import read_image
 
 # import Mask2Former project
 from mask2former import add_maskformer2_config
 from mlflow_config import add_mlflow_config
+from train_dinat import add_dinat_config
+
+from detectron2.config import get_cfg
+from detectron2.data.detection_utils import read_image
+
+# import some common detectron2 utilities
+from detectron2.engine import DefaultPredictor
+from detectron2.projects.deeplab import add_deeplab_config
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config-file", type=str)
     parser.add_argument("--weight", type=str)
-    arg = parser.parse_args()
-    return arg
+    return parser.parse_args()
 
 
 def setup_cfg(args):
     cfg = get_cfg()
     add_deeplab_config(cfg)
     add_maskformer2_config(cfg)
+    add_dinat_config(cfg)  # this is only for dinat model
     add_mlflow_config(cfg)
     cfg.merge_from_file(args.config_file)
     cfg.MODEL.WEIGHTS = args.weight
@@ -56,22 +60,20 @@ def main():
     size = 256
     transform = A.Compose([A.Resize(size, size)])
     image_id = []
-    preds_array = np.empty((0, size * size), dtype=np.long)
-    for index, image_info in enumerate(tqdm(images, total=len(images))):
+    preds_array = np.empty((0, size**2), dtype=np.long)
+    for image_info in tqdm(images, total=len(images)):
         file_name = image_info["file_name"]
         image_id.append(file_name)
         path = Path("/opt/ml/input/data") / file_name
         img = read_image(path, format="BGR")
         pred = predictor(img)
         output = pred["sem_seg"].argmax(dim=0).detach().cpu().numpy()
-        temp_mask = []
         temp_img = np.zeros((3, 512, 512))
         transformed = transform(image=temp_img, mask=output)
         mask = transformed["mask"]
-        temp_mask.append(mask)
-
+        temp_mask = [mask]
         oms = np.array(temp_mask)
-        oms = oms.reshape([oms.shape[0], size * size]).astype(int)
+        oms = oms.reshape([oms.shape[0], size**2]).astype(int)
         preds_array = np.vstack((preds_array, oms))
 
     submission = pd.read_csv(
@@ -87,7 +89,12 @@ def main():
             ignore_index=True,
         )
     create_dir("./submission")
-    submission.to_csv("./submission/no_back_submission.csv", index=False)
+    sub_name = (
+        cfg.MODEL.BACKBONE.NAME[2:]
+        + "_iter_"
+        + re.findall("\d+", cfg.MODEL.WEIGHTS.split("/")[-1])[0].lstrip("0")
+    )
+    submission.to_csv(f"./submission/{sub_name}.csv", index=False)
 
 
 def create_dir(directory):
