c167f48b7197438a4e4d1b783be484b9bfea4f98
JuHeon-Oh <ojhgold@gmail.com>
2023-01-01 15:54:24 +0900

    add: BatchPredictor closed #17 #29
    1. BatchPredictor 구현, 기존에는 single image로 infer 해서 시간이 오래걸림
    2. Batch 단위에서 한 번에 Resize를 위해 Kornia 적용
    3. submission하는 for loop의 O(256*256*819) 해결
    -> " ".join(map(str, string.tolist()))

--- Mask2Former/inference.py
+++ Mask2Former/inference.py
@@ -1,7 +1,5 @@
 import sys
 
-import cv2
-
 sys.path.insert(0, "Mask2Former")
 
 import argparse
@@ -18,9 +16,17 @@ from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
 
+from typing import List, Optional, Tuple
+
+import cv2
+import kornia as K
+import torch
+import torch.nn as nn
+
 # import Mask2Former project
 from mask2former import add_maskformer2_config
 from mlflow_config import add_mlflow_config
+from torch.utils.data import DataLoader, Dataset
 from train_dinat import add_dinat_config
 
 from detectron2.config import get_cfg
@@ -28,6 +34,7 @@ from detectron2.data.detection_utils import read_image
 
 # import some common detectron2 utilities
 from detectron2.engine import DefaultPredictor
+from detectron2.modeling import build_model
 from detectron2.projects.deeplab import add_deeplab_config
 
 
@@ -50,51 +57,109 @@ def setup_cfg(args):
     return cfg
 
 
+class ImageDataset(Dataset):
+    def __init__(self, image: List[Path]):
+        self.image = image
+
+    def __getitem__(self, index) -> np.ndarray:
+        return self.image[index]
+
+    def __len__(self):
+        return len(self.image)
+
+
+class BatchPredictor(DefaultPredictor):
+    """Run batch inference with D2"""
+
+    def __collate(self, batch):
+        data = []
+        for image in batch:
+            # Apply pre-processing to image.
+            if self.input_format == "RGB":
+                # whether the model expects BGR inputs or RGB
+                image = image[:, :, ::-1]
+            height, width = image.shape[:2]
+
+            # image = self.aug.get_transform(image).apply_image(image)
+            image = image.astype("float32").transpose(2, 0, 1)
+            image = torch.as_tensor(image)
+            data.append({"image": image, "height": height, "width": width})
+        return data
+
+    def __call__(self, img_infos: List[dict]) -> List[dict]:
+        """Run d2 on a list of images.
+
+        Args:
+            images (list): BGR images of the expected shape: 720x1280
+        """
+
+        images = []
+        image_id = []
+        for img_info in img_infos:
+            path = Path("/opt/ml/input/data") / img_info["file_name"]
+            img = read_image(path, format="BGR")
+            images.append(img)
+            image_id.append(img_info["file_name"])
+        dataset = ImageDataset(images)
+        loader = DataLoader(
+            dataset,
+            batch_size=32,
+            shuffle=False,
+            num_workers=4,
+            collate_fn=self.__collate,
+            pin_memory=True,
+        )
+        size = 256
+        transform_k = nn.Sequential(K.geometry.Resize((size, size)))
+        preds_array = np.empty((0, size**2), dtype=np.long)
+
+        with torch.no_grad():
+            for batch in tqdm(loader, total=len(loader)):
+                outs = self.model(batch)
+                outs = torch.cat(
+                    [out["sem_seg"].unsqueeze(0) for out in outs], 0
+                ).argmax(1)
+                masks = transform_k(outs.type(torch.FloatTensor)).detach().cpu().numpy()
+                oms = masks.reshape([len(batch), -1]).astype(int)
+                preds_array = np.vstack((preds_array, oms))
+                del masks, oms, outs
+                torch.cuda.empty_cache()
+
+        return image_id, preds_array
+
+
 def main():
     args = get_parser()
     cfg = setup_cfg(args)
     with open("/opt/ml/input/data/test.json") as f:
         test_files = json.load(f)
     images = test_files["images"]
-    predictor = DefaultPredictor(cfg)
+    predictor = BatchPredictor(cfg)
     size = 256
-    transform = A.Compose([A.Resize(size, size)])
     image_id = []
     preds_array = np.empty((0, size**2), dtype=np.long)
-    for image_info in tqdm(images, total=len(images)):
-        file_name = image_info["file_name"]
-        image_id.append(file_name)
-        path = Path("/opt/ml/input/data") / file_name
-        img = read_image(path, format="BGR")
-        pred = predictor(img)
-        output = pred["sem_seg"].argmax(dim=0).detach().cpu().numpy()
-        temp_img = np.zeros((3, 512, 512))
-        transformed = transform(image=temp_img, mask=output)
-        mask = transformed["mask"]
-        temp_mask = [mask]
-        oms = np.array(temp_mask)
-        oms = oms.reshape([oms.shape[0], size**2]).astype(int)
-        preds_array = np.vstack((preds_array, oms))
-
+    print("Start inference")
+    image_id, preds_array = predictor(images)
+    print("End inference")
     submission = pd.read_csv(
         "/opt/ml/input/code/submission/sample_submission.csv", index_col=None
     )
-
-    for file_name, string in zip(image_id, preds_array):
+    print("Start submission")
+    for file_name, string in zip(tqdm(image_id), preds_array):
         submission = submission.append(
             {
                 "image_id": file_name,
-                "PredictionString": " ".join(str(e) for e in string.tolist()),
+                "PredictionString": " ".join(map(str, string.tolist())),
             },
             ignore_index=True,
         )
     create_dir("./submission")
-    sub_name = (
-        cfg.MODEL.BACKBONE.NAME[2:]
-        + "_iter_"
-        + re.findall("\d+", cfg.MODEL.WEIGHTS.split("/")[-1])[0].lstrip("0")
-    )
+    weight_nm = cfg.MODEL.WEIGHTS.split("/")[-1].split(".")[0]
+    regex_nm = re.findall("\d+", weight_nm)
+    nm = regex_nm[0].lstrip("0") or f"{weight_nm}"
+    sub_name = cfg.MODEL.BACKBONE.NAME[2:] + "_iter_" + nm
     submission.to_csv(f"./submission/{sub_name}.csv", index=False)
+    print("End submission! file name: ", f"{sub_name}.csv")
 
 
 def create_dir(directory):
