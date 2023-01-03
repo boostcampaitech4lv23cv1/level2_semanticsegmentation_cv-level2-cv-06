a69b9c03d2d34cb33c17055e92e3f859d11e5c7c
sshinohs <sshinohs@gmail.com>
2023-01-03 00:15:35 +0000

    add: reference of soft_voting

--- Mask2Former/inference_pkl.py
+++ Mask2Former/inference_pkl.py
@@ -2,6 +2,8 @@ import os
 import pickle
 import sys
 
+# https://github.com/boostcampaitech3/level2-semantic-segmentation-level2-cv-05/blob/main/utils/soft_voting.py
+
 sys.path.insert(0, "Mask2Former")
 
 from pathlib import Path
@@ -11,7 +13,7 @@ import json
 from tqdm import tqdm
 import warnings
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 # import some common detectron2 utilities
 from detectron2.engine import DefaultPredictor
@@ -25,14 +27,14 @@ from mask2former import add_maskformer2_config
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model-dir', type=str)
-    parser.add_argument('--weight', type=str, default='model_final.pth')
+    parser.add_argument("--model-dir", type=str)
+    parser.add_argument("--weight", type=str, default="model_final.pth")
     arg = parser.parse_args()
     return arg
 
 
 def setup_cfg(args):
-    config_dir = os.path.join(args.model_dir, 'config.yaml')
+    config_dir = os.path.join(args.model_dir, "config.yaml")
     weight_dir = os.path.join(args.model_dir, args.weight)
     cfg = get_cfg()
     add_deeplab_config(cfg)
@@ -46,19 +48,21 @@ def setup_cfg(args):
 def main():
     args = get_parser()
     cfg = setup_cfg(args)
-    with open('/opt/ml/input/data/test.json') as f:
+    with open("/opt/ml/input/data/test.json") as f:
         test_files = json.load(f)
-    images = test_files['images']
+    images = test_files["images"]
     predictor = DefaultPredictor(cfg)
     outputs = []
     for index, image_info in enumerate(tqdm(images, total=len(images))):
-        file_name = image_info['file_name']
-        path = Path('/opt/ml/input/data') / file_name
+        file_name = image_info["file_name"]
+        path = Path("/opt/ml/input/data") / file_name
         img = read_image(path, format="BGR")
         pred = predictor(img)
-        output = pred['sem_seg'].unsqueeze(dim=0).detach().cpu().numpy().astype(np.float16)
+        output = (
+            pred["sem_seg"].unsqueeze(dim=0).detach().cpu().numpy().astype(np.float16)
+        )
         outputs.append(output)
-    with open(f'./result_pkl/{args.model_dir}.pkl', 'wb') as f:
+    with open(f"./result_pkl/{args.model_dir}.pkl", "wb") as f:
         pickle.dump(outputs, f, pickle.HIGHEST_PROTOCOL)
 
 
--- utils/soft_voting.py
+++ utils/soft_voting.py
@@ -1,7 +1,9 @@
 import warnings
 from glob import glob
 
-warnings.filterwarnings('ignore')
+# https://github.com/boostcampaitech3/level2-semantic-segmentation-level2-cv-05/blob/main/utils/soft_voting.py
+
+warnings.filterwarnings("ignore")
 
 import numpy as np
 import pickle
@@ -10,19 +12,19 @@ import json
 import pandas as pd
 from tqdm import tqdm
 
-pkls = glob('./result_pkl/*.pkl')
+pkls = glob("./result_pkl/*.pkl")
 transform = A.Compose([A.Resize(256, 256)])
 
 
 def main():
-    with open('../data/test.json') as f:
+    with open("../data/test.json") as f:
         test_files = json.load(f)
-    images = test_files['images']
+    images = test_files["images"]
 
     n = len(pkls)
     output = np.zeros((624, 11, 512, 512), dtype=np.float16)
     for pkl in pkls:
-        output += np.array(pickle.load(open(pkl, 'rb')))
+        output += np.array(pickle.load(open(pkl, "rb")))
     output /= n
     output = np.argmax(output, axis=1)
 
@@ -30,24 +32,28 @@ def main():
     image_id = []
     preds_array = np.empty((0, size * size), dtype=np.long)
     for image, mask in tqdm(zip(images, output), total=624):
-        file_name = image['file_name']
+        file_name = image["file_name"]
         image_id.append(file_name)
         temp = np.zeros((1, 1, 1))
         transformed = transform(image=temp, mask=mask)
         temp_mask = []
-        transformed_mask = transformed['mask']
+        transformed_mask = transformed["mask"]
         temp_mask.append(transformed_mask)
 
         oms = np.array(temp_mask)
         oms = oms.reshape([oms.shape[0], size * size]).astype(int)
         preds_array = np.vstack((preds_array, oms))
 
-    submission = pd.read_csv('../code/submission/sample_submission.csv', index_col=None)
+    submission = pd.read_csv("../code/submission/sample_submission.csv", index_col=None)
 
     for file_name, string in zip(image_id, preds_array):
         submission = submission.append(
-            {"image_id": file_name, "PredictionString": ' '.join(str(e) for e in string.tolist())},
-            ignore_index=True)
+            {
+                "image_id": file_name,
+                "PredictionString": " ".join(str(e) for e in string.tolist()),
+            },
+            ignore_index=True,
+        )
 
     submission.to_csv(f"./soft_voting.csv", index=False)
 
